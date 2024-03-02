6476aad09d307480b787a10bcd93f8b889d9dd21
Mehmet Emin <mteminn@gmail.com>
2024-03-02 20:05:57 +0300

    remove unneccesary files

--- src/app/hooks/useSupabase.ts
+++
@@ -1,8 +0,0 @@
-import { useMemo } from 'react';
-import { getSupabaseBrowserClient } from '../lib/browser-client'
-
-function useSupabase() {
-  return useMemo(getSupabaseBrowserClient, []);
-}
-
-export default useSupabase;
