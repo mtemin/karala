7ac52962b4eab10849872ddb367b31c8c0dd8b6e
Mehmet Emin <mteminn@gmail.com>
2024-03-05 00:58:28 +0300

    this component now takes whole user object instead of just name

--- src/app/_components/username.tsx
+++ src/app/_components/username.tsx
@@ -1,28 +1,26 @@
-"use client"
-import React, { useState } from 'react';
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import CounterObserver from './counter';
+import { useEffect } from "react";
+import { currentUserAtom } from "../_stateStore/atoms";
+import { useAtom } from "jotai/react";
 
 type Props = {
-    username: string
+    user: User,
+
 }
-function Username({ username }: Props) {
-    // if (!username) return <div className="skeleton mb-1 rounded w-full h-6"></div>
+function Username({ user }: Props) {
+    if (!user) return <div className="skeleton mb-1 rounded w-full h-6"></div>
     return (
         <div className="user flex truncate">
             <p className="truncate">
-                {username
-                    ? username
+                {user
+                    ? user.given_name
                     : "Lorem Ipsum"}
+                {/* {user.name} */}
 
             </p>
             <p className='max-lg:hidden max-sm:inline'>
                 &apos;s &nbsp;Notes
             </p>
-            {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-auto">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
-            </svg> */}
-
         </div>
     );
 }
