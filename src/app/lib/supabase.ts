28201b382e72a111153490268e2e5fdba19e6d6b
Mehmet Emin <mteminn@gmail.com>
2024-03-06 14:23:12 +0300

    fix import after folder rename

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { useQuery } from '@tanstack/react-query';
 import { getNotes } from '../queries/getNotes';
-import useNoteQuery from '../hooks/useNoteQuery';
+import useNoteQuery from '../_hooks/useNoteQuery';
 const supabaseUrl = 'https://cflyjcqujlqbmknzxoei.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmbHlqY3F1amxxYm1rbnp4b2VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ0NDgyNTMsImV4cCI6MjAyMDAyNDI1M30.dbpzkERMOY2FSl2fx4p59ftJI6df-TogrwGuVwD9qew';
 
