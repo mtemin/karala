ff5afc74f1ae1fff78d42904de24da1d3f8e7a25
Mehmet Emin <mteminn@gmail.com>
2024-03-08 11:37:54 +0300

    remove unneccesary atom, format export object

--- src/app/_stateStore/atoms.ts
+++ src/app/_stateStore/atoms.ts
@@ -1,17 +1,25 @@
 import { atom } from 'jotai';
 
-// const currentNoteAtom = ({
-//   "title": "New Title",
-//   "description": "What is on your mind"
-// });
 const currentNoteIdAtom = atom<number | null>(null)
+const settingsModalAtom = atom<boolean>(false);
 const currentTitleAtom = atom<string>("");
 const currentDescriptionAtom = atom<string>("");
 const currentUserAtom = atom<object | null>(null);
 const searchAtom = atom<string>("");
 const noteListAtom = atom<Note[]>([]);
 const onlineAtom = atom<boolean>(false);
-const themeAtom = atom<string>("default");
+const themeAtom = atom<string | null>("default");
 const isSidebarVisibleAtom = atom<boolean>(true);
 
-export { currentTitleAtom, currentDescriptionAtom, currentUserAtom, searchAtom, noteListAtom, onlineAtom, themeAtom, currentNoteIdAtom, isSidebarVisibleAtom }
+export {
+  currentTitleAtom,
+  currentDescriptionAtom,
+  currentUserAtom,
+  searchAtom,
+  noteListAtom,
+  onlineAtom,
+  themeAtom,
+  currentNoteIdAtom,
+  isSidebarVisibleAtom,
+  settingsModalAtom
+}
