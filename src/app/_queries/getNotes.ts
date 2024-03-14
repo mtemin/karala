c41bdc388f724917124415807d57e5bfa62815f9
Mehmet Emin <mteminn@gmail.com>
2024-03-14 17:40:45 +0300

    refactor code, delete unneccesary lines

--- src/app/_queries/getNotes.ts
+++ src/app/_queries/getNotes.ts
@@ -1,17 +1,10 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { useAtom } from "jotai/react";
-import { currentUserAtom } from "../_stateStore/atoms";
-// import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
 
-export async function getNotes(client: any, userId: string) {
-  // const [currentUser, setCurrentUser] = useAtom(currentUserAtom);
-  // console.log(currentUser);
-
+export async function getNotes(client: any, userId: string, isArchived: boolean) {
   return await client
     .from('notes')
     .select('*')
     .eq('user_id', userId)
+    .eq('isArchived', isArchived)
     .order('created_at', { ascending: true })
     .throwOnError();
-  // }
 }
--- src/app/_queries/getUser.ts
+++ src/app/_queries/getUser.ts
@@ -1,13 +1,9 @@
-"use server"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { useAtom } from "jotai/react";
 import { currentUserAtom } from "../_stateStore/atoms";
 
 export async function getUser() {
-  const [currentUser, setCurrentUser] = useAtom(currentUserAtom);
   const { getUser } = getKindeServerSession();
   const user = await getUser();
-  setCurrentUser(user);
-
   return user;
 }
