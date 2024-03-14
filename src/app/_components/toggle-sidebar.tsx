df6f28dcd08559b708b3c4477eea261d35251f28
Mehmet Emin <mteminn@gmail.com>
2024-03-14 17:47:37 +0300

    fix classname passing to avoid daisyUI error

--- src/app/_components/toggle-sidebar.tsx
+++ src/app/_components/toggle-sidebar.tsx
@@ -1,12 +1,13 @@
 "use client"
 import { useAtom } from 'jotai';
-import LeftArrowIcon from './i-left-arrow'
+import LeftArrowIcon from './icons/i-left-arrow'
 import { isSidebarVisibleAtom } from '../_stateStore/atoms';
 
 export default function ToggleSidebar() {
 
   const [isSidebarVisible, setIsSidebarVisible] = useAtom(isSidebarVisibleAtom);
-  const iconStyle = isSidebarVisible ? "" : "rotate-180";
+  // const iconStyle = isSidebarVisible ? "" : "rotate-180";
+  const iconStyle = isSidebarVisible ? "w-4 h-4 transition-all duration-700" : " w-4 h-4 transition-all duration-700 rotate-180";
 
   return (
     <div onClick={() => {
@@ -16,8 +17,8 @@ export default function ToggleSidebar() {
         setIsSidebarVisible(!isSidebarVisible);
       }
     }}
-      className="cursor-pointer tooltip tooltip-right p-1 bg-base-200 absolute right-[-18px] top-10 border-[--foreground] border-2 rounded-full transition-all duration-100 hover:bg-neutral" data-tip="Toggle sidebar">
-      <LeftArrowIcon className={`w-4 h-4 transition-all duration-700 ${iconStyle}`} isSidebarVisible={isSidebarVisible} />
+      className="z-[11] cursor-pointer tooltip tooltip-right p-1 bg-base-200 absolute right-[-18px] top-10 border-[--foreground] border-2 rounded-full transition-all duration-100 hover:bg-neutral" data-tip="Toggle sidebar">
+      <LeftArrowIcon className={iconStyle} isSidebarVisible={isSidebarVisible} />
     </div>
   )
 }
