a3ae802a784dbb9bb82eaefc904281af9600ef3b
Mehmet Emin <mteminn@gmail.com>
2024-03-14 17:39:45 +0300

    added alert atom and reformatted the file

--- src/app/_stateStore/atoms.ts
+++ src/app/_stateStore/atoms.ts
@@ -1,15 +1,19 @@
 import { atom } from 'jotai';
 
-const currentNoteIdAtom = atom<number | null>(null)
 const settingsModalAtom = atom<boolean>(false);
+const archiveModalAtom = atom<boolean>(false);
+const isSidebarVisibleAtom = atom<boolean>(true);
+
+const currentNoteIdAtom = atom<number | null>(null)
 const currentTitleAtom = atom<string>("");
 const currentDescriptionAtom = atom<string>("");
 const currentUserAtom = atom<object | null>(null);
+
 const searchAtom = atom<string>("");
 const noteListAtom = atom<Note[]>([]);
 const onlineAtom = atom<boolean>(false);
 const themeAtom = atom<string | null>("default");
-const isSidebarVisibleAtom = atom<boolean>(true);
+const alertAtom = atom<boolean>(false);
 
 export {
   currentTitleAtom,
@@ -21,5 +25,7 @@ export {
   themeAtom,
   currentNoteIdAtom,
   isSidebarVisibleAtom,
-  settingsModalAtom
+  settingsModalAtom,
+  archiveModalAtom,
+  alertAtom
 }
