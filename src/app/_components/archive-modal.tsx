7782f0a6fabcbe2c8d98c290fef9cea0736af534
Mehmet Emin <mteminn@gmail.com>
2024-03-16 15:36:10 +0300

    was too small in mobile

--- src/app/_components/archive-modal.tsx
+++ src/app/_components/archive-modal.tsx
@@ -12,7 +12,7 @@ export default function Archive() {
   return (
     <div id="archiveModal" className={archiveModal ? '' : 'hidden'}>
       <div className="z-[9] fixed top-0 left-0 w-full h-full bg-base-200 opacity-70"></div>
-      <div className='z-10 translate-x-[-50%] flex flex-col flex-wrap absolute rounded-xl p-8 max-w-[500px] sm:w-[80vw] h-[60vh] bg-base-300 left-[50vw] top-[25vh]'>
+      <div className='z-10 translate-x-[-50%] flex flex-col flex-wrap absolute rounded-xl p-8 max-w-[500px] max-sm:w-[80vw] h-[60vh] bg-base-300 left-[50vw] top-[25vh]'>
         <button onClick={() => { setArchiveModal(false) }}
           className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 text-xl">✕</button>
         <p className="text-base-content font-medium mb-4">Archived Notes :</p>
