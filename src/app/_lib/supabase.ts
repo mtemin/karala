4f3e3c412c4d8298a17c8aa372c18aa995042982
Mehmet Emin <mteminn@gmail.com>
2024-05-15 16:11:26 +0300

    removed commented line because vercel trying to compile it

--- src/app/_lib/supabase.ts
+++ src/app/_lib/supabase.ts
@@ -3,4 +3,3 @@ import { createClient } from '@supabase/supabase-js';
 // VERCEL
 //@ts-ignore
 export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, { auth: { persistSession: false } })
-// export const supabase = createClient(supabaseUrl, supabaseKey, { auth: { persistSession: false } });
