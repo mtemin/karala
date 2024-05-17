9e0e734ef34b7ab6955577b86ed63f0b1bd1d150
Mehmet Emin <mteminn@gmail.com>
2024-05-17 22:00:40 +0300

    remove tooltips from flag icons and increase z index of modal to make it appear over the toggleSidebar icon/component

--- src/app/_components/SettingsModal.tsx
+++ src/app/_components/SettingsModal.tsx
@@ -8,8 +8,8 @@ export default function SettingsModal() {
   const [settingsModal, setSettingsModal] = useAtom(settingsModalAtom);
   return (
     <div id="settingsModal" className={settingsModal ? '' : 'hidden'}>
-      <div className="z-[9] fixed top-0 left-0 w-full h-full bg-base-200 opacity-70"></div>
-      <div className='z-10 translate-x-[-50%] flex justify-between flex-wrap absolute rounded-xl p-8 w-[500px] max-sm:w-[80vw] h-[60vh] bg-base-300 left-[50vw] top-[25vh]'>
+      <div className="z-[11] fixed top-0 left-0 w-full h-full bg-base-200 opacity-70"></div>
+      <div className='z-12 translate-x-[-50%] flex justify-between flex-wrap absolute rounded-xl p-8 w-[500px] max-sm:w-[80vw] h-[60vh] bg-base-300 left-[50vw] top-[25vh]'>
         <button onClick={() => { setSettingsModal(false) }}
           className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
         <ThemeControllerButtons />
@@ -18,12 +18,12 @@ export default function SettingsModal() {
           <p className="text-base-content font-medium">Language :</p>
           <div className="flags flex items-center ml-6">
 
-            <div className="tooltip cursor-pointer" data-tip="English">
+            {/*<div className="tooltip cursor-pointer" data-tip="English">*/}
               <img src="GB.svg" alt="en-flag" className='opacity-70 w-10 rounded' />
-            </div>
-            <div className="tooltip cursor-pointer  ml-6" data-tip="Turkish">
+            {/*</div>*/}
+            {/*<div className="tooltip cursor-pointer  ml-6" data-tip="Turkish">*/}
               <img src="TR.svg" alt="tr-flag" className='opacity-70 w-10 rounded' data-tip="Turkish" />
-            </div>
+            {/*</div>*/}
           </div>
         </div>
         <button onClick={() => { setSettingsModal(false) }}
