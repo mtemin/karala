d3a267adb0e877a9cba53738184a3a401879e2ce
Mehmet Emin <mteminn@gmail.com>
2024-05-27 16:28:54 +0300

    rename Alert and related files/functions as Notification

--- src/app/_components/Notification.tsx
+++ src/app/_components/Notification.tsx
@@ -1,12 +1,9 @@
 "use client"
 type Props = {
   type: "info" | "success" | "warning" | "error",
-  text: string
 }
 
-// const [alert, setAlert] = useAtom(alertAtom);
-
-export default function Alert({ type, text }: Props) {
+export default function Notification({ type }: Props) {
   let className;
   switch (type) {
     case "success":
@@ -26,7 +23,7 @@ export default function Alert({ type, text }: Props) {
   return (
     <div role="alert" className={className}>
       <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-      <span>{text}</span>
+      <span></span>
     </div>
   )
 }
--- src/app/_hooks/useActivateNotification.ts
+++ src/app/_hooks/useActivateNotification.ts
@@ -1,9 +1,11 @@
 
-export function activateAlert(type: string) {
+export function useActivateNotification(type: string, text:string) {
   let element: HTMLElement | null = document.querySelector(`.alert-${type}`);
   console.log(element)
-  if (element)
+  if (element){
     element.style.marginTop = "1rem"
+    element.innerText=text;
+  }
   setTimeout(() => {
     if (element)
       element.style.marginTop = "-7rem"
